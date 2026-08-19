c670b11162e4a7ee536f1888cf2df81292f02da8
hashjnsrnhxh <honeymielllll@gmail.com>
2026-08-19 20:29:05 -0300

    Update setup_sources.sh

--- scripts/setup_sources.sh
+++ scripts/setup_sources.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "Clonando repositorio base de emulación..."
+echo "Clonando repositorio base de Winlator..."
 git clone --depth 1 https://github.com/brunodev85/winlator.git temp_winlator
 
-echo "Copiando estructura del proyecto Android..."
-shopt -s dotglob
-cp -r temp_winlator/* .
-shopt -u dotglob
+echo "Copiando todos los archivos del proyecto..."
+cp -a temp_winlator/. .
 rm -rf temp_winlator
 
-if [ ! -f "gradlew" ]; then
+echo "Verificando ejecutable de Gradle..."
+if [ -f "gradlew" ]; then
+    chmod +x gradlew
+else
     echo "Generando ejecutable Gradle..."
     gradle wrapper
+    chmod +x gradlew
 fi
 
-chmod +x gradlew
 echo "Fuentes de Winlator listas para compilar."
